c2ff21e66b4cf5fe4a5daac78c78f044223f60c5
bbhqoma <157393055+bbhqoma@users.noreply.github.com>
2026-08-04 16:19:29 +0700

    spc

--- config.sh
+++ config.sh
@@ -6,10 +6,10 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="L80_S" # codename used in device tree
+export DT_LINK="https://github.com/bbhqoma/android_device_spc_L80_S/tree/L80_S-user-10-Q00711-1602229646-release-keys" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export VENDOR="spc" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
